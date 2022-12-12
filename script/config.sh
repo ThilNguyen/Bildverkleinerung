d0adfcb3b88e238c52581614165d6a21b3073293
thi_nguyen <thi.nguyen@ksb-sg.ch>
2022-12-12 11:50:52 +0100

    Thi 12.12.2022

---
+++ script/config.sh
@@ -0,0 +1,5 @@
+﻿{
+    "bucket_name"; "my-bike-gbs-bucket",
+    "lambda_name"; "image-converter",
+    "statement_id"; "s3-permission-statement-image-converter"
+}
---
+++ script/remove.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+
+bucket_name=`cat.config.json | jq '.bucket_name' | tr -d '"'`
+lambda_name=`cat config.json | jq '.lambda_name' | tr -d '"'`
+statement_id=`cat config.json | jq 'statement_id' | tr -d '"'`
+
+echo "Remove bucket: ${bucket_name}"
+aws s3 rb s3://${bucket_name} --force
+
+echo "Remove s3"   # weitermachen
