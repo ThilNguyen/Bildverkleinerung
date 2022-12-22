374e1f0e6a54b0506c5839f9a1170a82bb60b2d4
LivioPiccolotto <111734364+LivioPiccolotto@users.noreply.github.com>
2022-12-22 22:35:48 +0100

    Delete upload.sh

--- script/upload.sh
+++
@@ -1,8 +0,0 @@
-﻿#!/bin/bash
-
-bucket_name=`cat config.json | jq '.bucket_name' | tr -d '"'`
-bucket_arn="arn:aws:s3:::${bucket_name}"
-image_name="sample-image.png"
-
-echo "Upload file ${image_name} ..."
-aws s3 cp ${image_name} s3://${bucket_name}
